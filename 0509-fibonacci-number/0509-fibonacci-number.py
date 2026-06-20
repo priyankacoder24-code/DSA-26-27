2ed84de608323639bd5807425f643d6f523c6952
priyankacoder24-code <priyankacoder24@gmail.com>
2026-06-20 14:10:52 +0530

    Time: 312 ms (27.44%), Space: 19.1 MB (56.7%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        def fib(n):
+            if n <= 1:
+                return n
+            return fib(n - 1) + fib(n - 2)
+
+        return fib(n) 
+        
