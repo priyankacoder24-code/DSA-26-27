39a6890229b1843ec75a866126641a51812264db
priyankacoder24-code <priyankacoder24@gmail.com>
2026-06-22 13:57:59 +0530

    Time: 4 ms (12.25%), Space: 19.4 MB (5.95%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,18 +1,12 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        alltypes = {"]":"[" , "}" : "{" , ")" : "("}
         stack = []
-        pairs = {
-            ')': '(',
-            '}': '{',
-            ']': '['
-        }
 
-        for char in s:
-            if char in pairs.values():
-                stack.append(char)
-            else:  
-                if not stack or stack.pop() != pairs[char]:
-                    return False
-
-        return len(stack) == 0 
+        for c in s:
+            if stack and c in alltypes and stack[-1] == alltypes[c]:
+                stack.pop()
+            else:
+                stack.append(c)
+        return True if len(stack)==0 else False
         
