ef329abb51c407bb19c504db5fc1e3a26ee8d66a
priyankacoder24-code <priyankacoder24@gmail.com>
2026-07-31 13:12:43 +0530

    Time: 2 ms (24.98%), Space: 19.4 MB (16.75%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -0,0 +1,14 @@
+from math import gcd, isqrt
+
+class Solution:
+    def commonFactors(self, a: int, b: int) -> int:
+        g = gcd(a, b)
+        count = 0
+
+        for i in range(1, isqrt(g) + 1):
+            if g % i == 0:
+                count += 1
+                if i != g // i:
+                    count += 1
+
+        return count
